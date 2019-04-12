146335340df682787b8265ab56915b4743fe2565
M Alvarado <malvar06@baker.edu>
2019-04-12 16:45:59 -0400

    Added navbar to contact and landing page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -8,6 +8,37 @@
 
 </head>
 <body>
+	<nav>
+		<ul class="nav justify-content-center">
+			<li class="nav-item"><a class="nav-link" href="#">Home</a></li>			
+			<li class="nav-item"><a class="nav-link" href="#">Contact</a>
+		</ul>
+		<div class="nav-login">
+			<!-- display logout button if logged in, hide otherwise -->
+			<?php 
+				if(isset($_SESSION['loggedIn'])) { ?> 
+					<!-- display if loggedIn-->
+					<form action="#" method="POST">
+						<p>You are logged in.</p>
+						<input type="hidden" name="submitted" value="true">
+						<input class="btn btn-primary" type="submit" name="submit">Logout</input>
+					</form>
+				 <?php } else { ; ?>
+				<!-- Display; NOT loggedIn  -->
+				<form action="#" method="POST">
+					<!-- TODO: accept username or email for username -->
+					<input type="text" name="username" placeholder="Email" required>						
+					<input type="password" name="password" placeholder="password" required>
+					<input type="hidden" name="submitted" value="true">
+					<button class="btn btn-secondary" type="submit" name="submit">Login</button>
+					
+				</form>				
+				<form>
+					<button class="btn btn-warning"><a href="register.php"></a>Sign Up</button>	
+				</form>
+			 <?php }; ?>
+		</div>
+	</nav>
 
 	<h2>Contact</h2>
 	<p><span class="error">* required field</span></p>
